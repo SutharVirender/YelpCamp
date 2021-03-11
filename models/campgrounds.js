f5aaef6ed0b999e32da0028b04211a1f9770b323
SutharVirender <vksuthaar@gmail.com>
2021-03-11 19:29:11 +0530

    add reviews creation and Deletion

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,10 +1,25 @@
+const { object } = require("joi");
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
+const Review=require("./review");
 const campgroundschema=new Schema({
     title: String,
     location: String,
     price: Number,
     description: String,
-    img:String
+    img:String,
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:"Review"
+    }]
 });
+campgroundschema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 module.exports=mongoose.model('campground',campgroundschema)
